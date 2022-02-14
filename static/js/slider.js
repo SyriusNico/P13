45716c4b527cd3070245dec249abf3ffcfccd24e
Nicolas <pnschlei@gmail.com>
2022-02-14 05:20:20 +0100

    first commit

---
+++ static/js/slider.js
@@ -0,0 +1,20 @@
+let sliderContent = document.getElementsByClassName('content');
+
+let counter = 0;
+
+let numberOfSlide = sliderContent.length;
+
+function hideContent() {
+	for(let i = 0; i < numberOfSlide ; i++) {
+		sliderContent[i].classList.remove('active');
+	}
+};
+
+setInterval(function () {
+	counter++;
+	if(counter >= numberOfSlide) {
+		counter = 0;
+	}
+	hideContent();
+	sliderContent[counter].classList.add('active');
+}, 8000)
