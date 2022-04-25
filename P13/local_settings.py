c7867a80d8e3f00217a9c57a591eb746f75c47f7
Nicolas <pnschlei@gmail.com>
2022-04-25 21:50:07 +0200

    deploy to heroku

--- P13/local_settings.py
+++ P13/local_settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'P13.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'P13',
-        'USER': 'postgres',
-        'PASSWORD': 'popi',
-        'HOST': '',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'P13',
+#         'USER': 'postgres',
+#         'PASSWORD': 'popi',
+#         'HOST': '',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
