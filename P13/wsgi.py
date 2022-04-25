ab3f184bb245ea9fb175f7d62f6c1636cdd36743
Nicolas <pnschlei@gmail.com>
2022-04-25 17:33:40 +0200

    deploy to heroku

--- P13/wsgi.py
+++ P13/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'P13.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'P13.local_settings')
 
 application = get_wsgi_application()
